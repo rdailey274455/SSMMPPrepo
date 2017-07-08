77f9fbcc39230b2ba227fb2a7be109b6c1aa87fc
Rhys Dailey <rdailey274455@gmail.com>
2017-07-07 22:41:38 -0600

    Friday's work
    Started gameplay scene, wrote ideas

---
+++ SuperSovietMissileMastarPlusPlus/Assets/Scripts/SlowRotateY.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowRotateY : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(0f,1f,0f),1f);
+		// transform.RotateAround(transform.position,Vector3.up,1f);
+	}
+}
